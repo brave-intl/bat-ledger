69845410b7ee07e02cb5df2200dc62aa02764f43
Marshall T. Rose <mrose17@gmail.com>
2017-08-02 08:43:19 -0700

    0.9.5: package updates and correct package.json determination

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const path = require('path')
 const glob = require('glob')
 const underscore = require('underscore')
 
-const cwd = __dirname
+const cwd = process.cwd()
 const prefix = 'boot-'
 
 const parent = path.join(cwd, '..')
-const npminfo = require(path.join((parent !== 'node_modules') ? cwd : path.join(parent, '..'), 'package'))
+console.log(underscore.last(parent.split(path.sep)))
+const npminfo = require(path.join((underscore.last(parent.split(path.sep)) !== 'node_modules') ? cwd : path.join(parent, '..'),
+    'package'))
 process.npminfo = underscore.pick(npminfo,
                                   [ 'name', 'version', 'description', 'author', 'license', 'bugs', 'homepage', 'dependencies' ])
 
-glob.sync(prefix + '*.js', { cwd: cwd }).forEach((file) => {
+glob.sync(prefix + '*.js', { cwd: __dirname }).forEach((file) => {
   const key = path.basename(file.substring(prefix.length), '.js')
 
-  module.exports[key] = module.exports[key.charAt(0).toUpperCase() + key.slice(1)] = require(path.join(cwd, file))
+  module.exports[key] = module.exports[key.charAt(0).toUpperCase() + key.slice(1)] = require(path.join(__dirname, file))
 })
