d9b95038d0009abe933f8ccf4a374370f5aa9e82
Marshall T. Rose <mrose17@gmail.com>
2017-09-13 17:16:52 -0700

    add temporary /v2/address/{altcurrency}/{address}/balance

--- ledger/controllers/address.js
+++ ledger/controllers/address.js
@@ -1,3 +1,4 @@
+const BigNumber = require('bignumber.js')
 const boom = require('boom')
 const Joi = require('joi')
 const underscore = require('underscore')
@@ -7,6 +8,7 @@ const braveHapi = utils.extras.hapi
 const braveJoi = utils.extras.joi
 
 const v1 = {}
+const v2 = {}
 
 /*
    GET /v1/address/{address}/validate
@@ -62,8 +64,64 @@ v1.validate =
   }
 }
 
+/*
+   GET /v2/address/{altcurrency}/{address}/balance
+ */
+
+v2.balance =
+{ handler: (runtime) => {
+  return async (request, reply) => {
+    const debug = braveHapi.debug(module, request)
+    const address = request.params.address
+    const altcurrency = request.params.altcurrency
+    const pair = {}
+    const wallets = runtime.database.get('wallets', debug)
+    let balances, paymentId, state, wallet
+
+    pair['addresses.' + altcurrency] = address
+    wallet = await wallets.findOne(pair)
+    if (!wallet) return reply(boom.notFound('invalid altcurrency/address: ' + altcurrency + '/' + address))
+
+    balances = await runtime.wallet.balances(wallet)
+    if (!underscore.isEqual(balances, wallet.balances)) {
+      state = { $currentDate: { timestamp: { $type: 'timestamp' } }, $set: { balances: balances } }
+      await wallets.update({ paymentId: wallet.paymentId }, state, { upsert: true })
+
+      await runtime.queue.send(debug, 'wallet-report', underscore.extend({ paymentId: paymentId }, state.$set))
+    }
+
+    reply({
+      probi: balances.confirmed.toString(),
+      balance: new BigNumber(balances.confirmed).dividedBy(runtime.currency.alt2scale(wallet.altcurrency)).toFixed(4),
+      unconfirmed: new BigNumber(balances.unconfirmed).dividedBy(runtime.currency.alt2scale(wallet.altcurrency)).toFixed(4),
+      rates: runtime.currency.rates[altcurrency]
+    })
+  }
+},
+
+  description: 'Temporary until balance servers are operational',
+  tags: [ 'api' ],
+
+  validate: {
+    params: {
+      altcurrency: braveJoi.string().altcurrencyCode().required().description('the wallet currency'),
+      address: braveJoi.string().altcurrencyAddress('BAT').required().description('the wallet address')
+    }
+  },
+
+  response: {
+    schema: Joi.object().keys({
+      balance: Joi.number().min(0).optional().description('the (confirmed) wallet balance'),
+      unconfirmed: Joi.number().min(0).optional().description('the unconfirmed wallet balance'),
+      rates: Joi.object().optional().description('current exchange rates to various currencies'),
+      probi: braveJoi.string().numeric().optional().description('the wallet balance in probi')
+    })
+  }
+}
+
 module.exports.routes = [
-  braveHapi.routes.async().path('/v1/address/{address}/validate').whitelist().config(v1.validate)
+  braveHapi.routes.async().path('/v1/address/{address}/validate').whitelist().config(v1.validate),
+  braveHapi.routes.async().path('/v2/address/{altcurrency}/{address}/balance').config(v2.balance)
 ]
 
 module.exports.initialize = async (debug, runtime) => {
