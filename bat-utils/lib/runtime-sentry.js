9e6ec9cf8be95f4f96e21e13c3eaac7670addad4
eV <ev@7pr.xyz>
2017-09-25 19:23:43 +0000

    chain notifiers (notify both slack and sentry)

--- bat-utils/lib/runtime-sentry.js
+++ bat-utils/lib/runtime-sentry.js
@@ -5,6 +5,14 @@ const Sentry = function (config, runtime) {
 
   if (config.sentry) {
     Raven.config(config.sentry.dsn).install()
+
+    const chainNotify = runtime.notify
+    runtime.notify = (debug, payload) => {
+      if (chainNotify) {
+        chainNotify(debug, payload)
+      }
+      Raven.captureMessage(payload.text)
+    }
   } else if (process.env.NODE_ENV === 'production') {
     throw new Error('config.sentry undefined')
   }
