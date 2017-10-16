ff7437ddf59bd0fbe952bb921752707bc7ac1024
Marshall T. Rose <mrose17@gmail.com>
2017-10-16 13:53:00 -0700

    fix description of /v2/publishers/{publisher}/wallet

--- eyeshade/controllers/publishers.js
+++ eyeshade/controllers/publishers.js
@@ -339,7 +339,7 @@ v2.getWallet = {
     mode: 'required'
   },
 
-  description: 'Gets the balance for a verified publisher',
+  description: 'Gets wallet information for a verified publisher',
   tags: [ 'api' ],
 
   validate: {
