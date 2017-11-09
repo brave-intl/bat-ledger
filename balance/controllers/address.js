ca70f7d7cfee07b9d4b1fe590e77ef15ec868bd8
eV <ev@7pr.xyz>
2017-11-09 23:12:04 +0000

    Add a new balance server endpoint to cache ledger wallet info

--- balance/controllers/address.js
+++ balance/controllers/address.js
@@ -2,6 +2,8 @@ const BigNumber = require('bignumber.js')
 const Joi = require('joi')
 const UpholdSDK = require('@uphold/uphold-sdk-javascript')
 const boom = require('boom')
+const underscore = require('underscore')
+const SDebug = require('sdebug')
 
 const utils = require('bat-utils')
 const braveHapi = utils.extras.hapi
@@ -9,13 +11,14 @@ const braveJoi = utils.extras.joi
 
 BigNumber.config({ EXPONENTIAL_AT: 28 })
 
+const debug = new SDebug('balance')
 const v2 = {}
 
 /*
    GET /v2/card/BAT/{cardId}/balance
  */
 
-v2.balance =
+v2.batBalance =
 { handler: (runtime) => {
   return async (request, reply) => {
     const cardId = request.params.cardId
@@ -77,6 +80,72 @@ v2.balance =
   }
 }
 
+/*
+   GET /v2/wallet/{paymentId}/balance
+ */
+
+v2.walletBalance =
+{ handler: (runtime) => {
+  return async (request, reply) => {
+    const paymentId = request.params.paymentId
+    let fresh = false
+    let expireIn = process.env.BALANCE_CACHE_TTL_S || 60 // 1 minute default
+
+    let walletInfo = await runtime.cache.get(paymentId, 'ledgerBalance:walletInfo')
+    if (walletInfo) {
+      walletInfo = JSON.parse(walletInfo)
+    } else {
+      try {
+        const url = `${runtime.config.ledger.url}/v2/wallet/${paymentId}?refresh=true`
+        debug('GET', url)
+        walletInfo = await braveHapi.wreck.get(url, {
+          redirects: 3,
+          rejectUnauthorized: true,
+          timeout: (5 * 1000),
+          useProxyP: true
+        })
+        if (Buffer.isBuffer(walletInfo)) walletInfo = JSON.parse(walletInfo)
+      } catch (ex) {
+        if (ex.isBoom) {
+          return reply(ex)
+        } else {
+          return reply(boom.badImplementation('an unknown error has occured'))
+        }
+      }
+      fresh = true
+    }
+
+    const balances = underscore.pick(walletInfo, ['altcurrency', 'probi', 'balance', 'unconfirmed', 'rates'])
+
+    reply(balances)
+
+    if (fresh) {
+      runtime.cache.set(paymentId, JSON.stringify(walletInfo), { EX: expireIn }, 'ledgerBalance:walletInfo')
+    }
+  }
+},
+
+  description: 'Get the balance of a ledger wallet',
+  tags: [ 'api' ],
+
+  validate: {
+    params: {
+      paymentId: Joi.string().guid().required().description('identity of the wallet')
+    }
+  },
+
+  response: {
+    schema: Joi.object().keys({
+      altcurrency: Joi.string().required().description('the wallet currency'),
+      balance: Joi.number().min(0).required().description('the (confirmed) wallet balance'),
+      unconfirmed: Joi.number().min(0).required().description('the unconfirmed wallet balance'),
+      rates: Joi.object().optional().description('current exchange rates to various currencies'),
+      probi: braveJoi.string().numeric().required().description('the wallet balance in probi')
+    })
+  }
+}
+
 module.exports.routes = [
-  braveHapi.routes.async().path('/v2/card/BAT/{cardId}/balance').config(v2.balance)
+  braveHapi.routes.async().path('/v2/card/BAT/{cardId}/balance').config(v2.batBalance),
+  braveHapi.routes.async().path('/v2/wallet/{paymentId}/balance').config(v2.walletBalance)
 ]
