8da21e7eabbe2ba5b7a3a244fbaae5a073621001
eV <ev@7pr.xyz>
2017-11-14 22:22:01 +0000

    switch to constant time comparison for simple token auth

---
+++ test/bat-utils.unit.test.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import batUtils from '../bat-utils'
+import test from 'ava'
+const braveHapi = batUtils.extras.hapi
+
+test('bat-utils : isSimpleTokenValid', async t => {
+  t.false(braveHapi.isSimpleTokenValid([], ''))
+  t.false(braveHapi.isSimpleTokenValid(['foo'], 'bar'))
+  t.false(braveHapi.isSimpleTokenValid(['foo'], 'foobar'))
+  t.false(braveHapi.isSimpleTokenValid(['foobar'], 'foo'))
+
+  t.true(braveHapi.isSimpleTokenValid(['foo'], 'foo'))
+  t.true(braveHapi.isSimpleTokenValid(['foo', 'bar'], 'foo'))
+  t.true(braveHapi.isSimpleTokenValid(['foo', 'bar'], 'bar'))
+
+  t.throws(() => {
+    braveHapi.isSimpleTokenValid('foo', 'bar')
+  })
+  t.throws(() => {
+    braveHapi.isSimpleTokenValid(['foo'], ['bar'])
+  })
+})
