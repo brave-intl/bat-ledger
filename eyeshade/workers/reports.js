8bd1988d3c0415d9d3f9b35802a0093dd81a42ca
Marshall T. Rose <mrose17@gmail.com>
2017-12-20 16:20:58 -0800

    Better diagnostics when uphold parameters aren't present on a GET .../wallet
    And try to include youtube channel name (rather than just channelID) in
    csv reports

--- eyeshade/workers/reports.js
+++ eyeshade/workers/reports.js
@@ -366,6 +366,42 @@ const publisherCompare = (a, b) => {
   return braveHapi.domainCompare(a.publisher, b.publisher)
 }
 
+const labelize = async (debug, runtime, data) => {
+  const labels = {}
+  const owners = runtime.database.get('owners', debug)
+  const publishersC = runtime.database.get('publishers', debug)
+
+  for (let offset in data) {
+    const datum = data[offset]
+    const publisher = datum.publisher
+    let entry, owner, props
+
+    if (!publisher) continue
+
+    if (labels[publisher]) {
+      datum.publisher = labels[publisher]
+      continue
+    }
+
+    props = batPublisher.getPublisherProps(publisher)
+    labels[publisher] = publisher
+
+    if (props && props.publisherType) entry = await publishersC.findOne({ publisher: publisher })
+    if (entry) {
+      labels[publisher] = props.URL
+      if ((!entry.info) && (entry.owner)) {
+        owner = await owners.findOne({ owner: entry.owner })
+        if (owner) entry = owner
+      }
+
+      if (entry.info && entry.info.name) labels[publisher] = entry.info.name + ' <' + labels[publisher] + '>'
+    }
+    datum.publisher = labels[publisher]
+  }
+
+  return data
+}
+
 const publisherContributions = (runtime, publishers, authority, authorized, verified, format, reportId, summaryP, threshold,
                               usd) => {
   const scale = new BigNumber(runtime.currency.alt2scale(altcurrency) || 1)
@@ -545,6 +581,7 @@ const publisherSettlements = (runtime, entries, format, summaryP) => {
                                     underscore.omit(txn, [ 'hash', 'settlementId', 'created', 'modified' ]),
                                     { transactionId: txn.hash, timestamp: txn.created && dateformat(txn.created, datefmt) }))
       })
+      if (entries.length > 1) data.push([])
     }
   })
 
@@ -731,7 +768,7 @@ exports.workers = {
         })
       }
 
-      try { await file.write(utf8ify(json2csv({ data: data })), true) } catch (ex) {
+      try { await file.write(utf8ify(json2csv({ data: await labelize(debug, runtime, data) })), true) } catch (ex) {
         debug('reports', { report: 'report-publishers-contributions', reason: ex.toString() })
         file.close()
       }
@@ -794,7 +831,7 @@ exports.workers = {
         })
       }
 
-      try { await file.write(utf8ify(json2csv({ data: data })), true) } catch (ex) {
+      try { await file.write(utf8ify(json2csv({ data: await labelize(debug, runtime, data) })), true) } catch (ex) {
         debug('reports', { report: 'report-publishers-settlements', reason: ex.toString() })
         file.close()
       }
@@ -935,7 +972,11 @@ exports.workers = {
           fieldNames.push('counts', 'address')
         }
 
-        await file.write(utf8ify(json2csv({ data: data, fields: fields, fieldNames: fieldNames })), true)
+        await file.write(utf8ify(json2csv({
+          data: await labelize(debug, runtime, data),
+          fields: fields,
+          fieldNames: fieldNames
+        })), true)
       } catch (ex) {
         debug('reports', { report: 'report-publishers-statements', reason: ex.toString() })
         file.close()
@@ -1113,7 +1154,7 @@ exports.workers = {
         if (result.reason !== 'bulk loaded') result.daysInQueue = daysago(result.created)
         data.push(result)
 
-        if (!summaryP) {
+        if ((!summaryP) && (result.history)) {
           result.history.forEach((record) => {
             if (elideP) {
               if (record.email) record.email = 'yes'
@@ -1162,7 +1203,7 @@ exports.workers = {
       const summaryP = payload.summary
       const settlements = runtime.database.get('settlements', debug)
       const voting = runtime.database.get('voting', debug)
-      let data, fields, file, previous, results, slices, publishers
+      let data, fields, file, mixerP, previous, results, slices, publishers
 
       if (!summaryP) {
         previous = await settlements.aggregate([
@@ -1188,6 +1229,22 @@ exports.workers = {
       }
 
       data = underscore.sortBy(await quanta(debug, runtime, undefined), 'created')
+      if (!summaryP) {
+        for (let quantum of data) {
+          slices = await voting.find({ surveyorId: quantum.surveyorId, exclude: false })
+          for (let slice of slices) {
+            if (slice.probi) continue
+
+            mixerP = true
+            break
+          }
+
+          if (mixerP) break
+        }
+
+        if (mixerP) await mixer(debug, runtime, undefined, undefined)
+      }
+
       results = []
       for (let quantum of data) {
         quantum = underscore.extend(quantum, {
@@ -1203,7 +1260,7 @@ exports.workers = {
         slices.forEach((slice) => {
           let probi
 
-          slice.probi = new BigNumber(slice.probi.toString())
+          slice.probi = new BigNumber(slice.probi ? slice.probi.toString() : '0')
           if (publishers[slice.publisher]) {
             probi = new BigNumber(publishers[slice.publisher].probi.toString())
 
@@ -1246,7 +1303,9 @@ exports.workers = {
       fields = [ 'surveyorId', 'probi', 'fee', 'inputs', 'quantum' ]
       if (!summaryP) fields.push('publisher')
       fields = fields.concat([ 'votes', 'created', 'modified' ])
-      try { await file.write(utf8ify(json2csv({ data: results, fields: fields })), true) } catch (ex) {
+      try {
+        await file.write(utf8ify(json2csv({ data: await labelize(debug, runtime, results), fields: fields })), true)
+      } catch (ex) {
         debug('reports', { report: 'report-surveyors-contributions', reason: ex.toString() })
         file.close()
       }
