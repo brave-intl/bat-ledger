85778c6c56ae7eff39b9048c74c7e5e63c6f1cb0
Marshall T. Rose <mrose17@gmail.com>
2018-01-04 04:49:00 -0800

    If not in production, use process.exit

--- bat-utils/lib/runtime-currency.js
+++ bat-utils/lib/runtime-currency.js
@@ -340,6 +340,7 @@ const maintenance = async (config, runtime) => {
   if (flatlineP) {
     debug('maintenance', { message: 'no trades reported' })
     runtime.captureException(new Error('maintenance reports flatline'))
+    if (process.env.NODE_ENV !== 'production') process.exit(0)
   }
   flatlineP = true
 
