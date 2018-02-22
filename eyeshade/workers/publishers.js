9a0dcda087925f6960d7af15a149b89011646dd4
Marshall T. Rose <mrose17@gmail.com>
2018-02-22 15:48:13 -0800

    eyeshade on startup and once daily ...
    … transmits entire verified publisher list to ledger, to ensure proper
    synchronization

--- eyeshade/workers/publishers.js
+++ eyeshade/workers/publishers.js
@@ -63,7 +63,7 @@ exports.workers = {
 
           if (entry.message === 'success') {
             await runtime.queue.send(debug, 'publisher-report',
-                                     { publisher: entry.publisher, verified: true, visible: visible })
+                                     { owner: entry.owner, publisher: entry.publisher, verified: true, visible: visible })
           }
         } catch (ex) {
           entry.message = ex.toString()
