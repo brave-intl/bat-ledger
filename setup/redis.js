735944961c0393f3145fcb0a8b80cab3228ba2bd
Michael McLaughlin <mmclaughlin@brave.com>
2018-04-11 10:15:20 -0500

    added setup scripts to help bootstrap the process

---
+++ setup/redis.js
@@ -0,0 +1,10 @@
+const redis = require("redis")
+const client = redis.createClient()
+
+client.flushdb((err, succeeded) => {
+  if (err) {
+    console.error(err)
+    return process.exit(1)
+  }
+  process.exit(0)
+})
