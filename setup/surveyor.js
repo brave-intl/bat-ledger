0fce9ef74b0ef10030ea98cdfde9c914a140a0e9
Michael McLaughlin <mmclaughlin@brave.com>
2018-05-01 16:45:10 -0700

    refactored to allow for passing in of domain

--- setup/surveyor.js
+++ setup/surveyor.js
@@ -1,4 +1,4 @@
 const curl = require('./curl')
 const data = {'adFree': {'fee': {'USD': 5}, 'votes': 5, 'altcurrency': 'BAT', 'probi': '27116311373482831368'}}
-const pathname = '/v2/surveyor/contribution'
-curl({pathname}, data).then(() => process.exit(0), () => process.exit(1))
+const path = '/v2/surveyor/contribution'
+curl({path}, data).then(() => process.exit(0), () => process.exit(1))
