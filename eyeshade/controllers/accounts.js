4885b146f99a16fd0c9d69198d3b26b64546da43
Michael McLaughlin <mmclaughlin@brave.com>
2018-09-04 15:53:54 -0500

    called boom fn

--- eyeshade/controllers/accounts.js
+++ eyeshade/controllers/accounts.js
@@ -77,6 +77,7 @@ v1.getBalances =
 { handler: (runtime) => {
   return async (request, reply) => {
     let accounts = request.query.account
+    if (!accounts) return reply(boom.badData())
 
     if (!Array.isArray(accounts)) {
       accounts = [accounts]
