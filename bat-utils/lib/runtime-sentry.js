49d613bbd6817febf2cbdcdcf626992c28b44b0d
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2018-09-04 15:56:37 -0500

    Send git commit info to sentry (#376)

--- bat-utils/lib/runtime-sentry.js
+++ bat-utils/lib/runtime-sentry.js
@@ -6,6 +6,8 @@ const underscore = require('underscore')
 
 const debug = new SDebug('sentry')
 
+const release = process.env.HEROKU_SLUG_COMMIT || 'test'
+
 const Sentry = function (config, runtime) {
   if (!(this instanceof Sentry)) return new Sentry(config, runtime)
 
@@ -20,6 +22,7 @@ const Sentry = function (config, runtime) {
   // NOTE If sentry dsn if falsey, events will be consumed without error
   //      with no attempt to send them
   Raven.config(config.sentry.dsn, {
+    release,
     captureUnhandledRejections: true
   }).install()
 
