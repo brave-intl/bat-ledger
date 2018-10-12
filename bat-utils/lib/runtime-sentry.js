f1fba9163fae0fb9f2ed48743d0b3cc777607e8d
Michael McLaughlin <mmclaughlin@brave.com>
2018-10-12 15:53:57 -0500

    refactored for upgrade

--- bat-utils/lib/runtime-sentry.js
+++ bat-utils/lib/runtime-sentry.js
@@ -1,13 +1,16 @@
 const { URL } = require('url')
-
-const Raven = require('raven')
+const Raven = require('@sentry/node')
 const SDebug = require('sdebug')
 const underscore = require('underscore')
 
 const debug = new SDebug('sentry')
 
-const Sentry = function (config, runtime) {
-  if (!(this instanceof Sentry)) return new Sentry(config, runtime)
+module.exports = Sentry
+
+function Sentry (config, runtime) {
+  if (!(this instanceof Sentry)) {
+    return new Sentry(config, runtime)
+  }
 
   const { sentry } = config
   const {
@@ -17,26 +20,27 @@ const Sentry = function (config, runtime) {
   } = sentry
 
   if (!dsn) {
-    process.on('unhandledRejection', (ex) => {
-      console.log(ex.stack)
-
-      debug('sentry', ex)
-    })
+    process.on('unhandledRejection', captureException)
+    process.on('uncaughtException', captureException)
   }
 
   const release = `${project}:${slug}`
   const enabled = !!dsn
   debug('sentry release', release)
-
-  // NOTE If sentry dsn if falsey, events will be consumed without error
-  //      with no attempt to send them
-  Raven.config(dsn, {
+  Raven.init({
+    dsn,
     enabled,
     release,
     captureUnhandledRejections: true
-  }).install()
+  })
 
-  runtime.captureException = (ex, optional) => {
+  runtime.captureException = captureException
+
+  if (!config.sentry && process.env.NODE_ENV === 'production') {
+    throw new Error('config.sentry undefined')
+  }
+
+  function captureException (ex, optional) {
     if (optional && optional.req) {
       const request = optional.req
       optional.req = { // If present rewrite the request into sentry format
@@ -47,15 +51,9 @@ const Sentry = function (config, runtime) {
       try {
         const url = new URL(request.path, runtime.config.server)
         if (url) optional.req['url'] = url
-      } catch (ex) { }
+      } catch (ex) {}
       optional.extra = underscore.extend(optional.extra, { timestamp: request.info.received, id: request.id })
     }
     Raven.captureException(ex, optional)
   }
-
-  if (!config.sentry && process.env.NODE_ENV === 'production') {
-    throw new Error('config.sentry undefined')
-  }
 }
-
-module.exports = Sentry
