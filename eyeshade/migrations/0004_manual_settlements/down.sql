fb2e395b31817e05786cf9eb3f1fe846cf546078
Nick von Pentz <nickvonpentz@gmail.com>
2019-02-14 18:59:33 -0500

    Fix migration 0004
    Add missing sql insert into migrations table.

--- eyeshade/migrations/0004_manual_settlements/down.sql
+++ eyeshade/migrations/0004_manual_settlements/down.sql
@@ -1,3 +1,4 @@
+delete from migrations where id = '0004';
 alter table transactions drop constraint check_transaction_type;
 alter table transactions add constraint check_transaction_type
   check (transaction_type in ('contribution', 'referral', 'contribution_settlement', 'referral_settlement', 'fees', 'scaleup', 'manual', 'ad', 'ad_settlement'));
--- eyeshade/migrations/0004_manual_settlements/up.sql
+++ eyeshade/migrations/0004_manual_settlements/up.sql
@@ -1,3 +1,4 @@
+insert into migrations (id, description) values ('0004', 'manual_settlements');
 alter table transactions drop constraint check_transaction_type;
 alter table transactions add constraint check_transaction_type
   check (transaction_type in ('contribution', 'referral', 'contribution_settlement', 'referral_settlement', 'fees', 'scaleup', 'manual', 'ad', 'ad_settlement', 'manual_settlement'));
