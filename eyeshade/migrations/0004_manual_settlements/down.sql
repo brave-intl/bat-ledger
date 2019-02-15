b4660065b8e24793b7aa45312329eb5e3a87f710
Nick von Pentz <nickvonpentz@gmail.com>
2019-02-14 22:38:40 -0500

    Only migrate 0004 down if it exists in migrations table

--- eyeshade/migrations/0004_manual_settlements/down.sql
+++ eyeshade/migrations/0004_manual_settlements/down.sql
@@ -1,4 +1,8 @@
+select execute($$
+
 delete from migrations where id = '0004';
 alter table transactions drop constraint check_transaction_type;
 alter table transactions add constraint check_transaction_type
   check (transaction_type in ('contribution', 'referral', 'contribution_settlement', 'referral_settlement', 'fees', 'scaleup', 'manual', 'ad', 'ad_settlement'));
+
+$$) where exists (select * from migrations where id = '0004');
