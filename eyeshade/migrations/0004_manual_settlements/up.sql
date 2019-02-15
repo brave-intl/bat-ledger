29ebd47f9aaf01ff585d36d0e93260a8bcf7562e
Nick von Pentz <nickvonpentz@gmail.com>
2019-02-14 20:35:22 -0500

    Only migrate 0004 up if it does not exist in migrations table

--- eyeshade/migrations/0004_manual_settlements/up.sql
+++ eyeshade/migrations/0004_manual_settlements/up.sql
@@ -1,4 +1,8 @@
+select execute($$
+
 insert into migrations (id, description) values ('0004', 'manual_settlements');
 alter table transactions drop constraint check_transaction_type;
 alter table transactions add constraint check_transaction_type
   check (transaction_type in ('contribution', 'referral', 'contribution_settlement', 'referral_settlement', 'fees', 'scaleup', 'manual', 'ad', 'ad_settlement', 'manual_settlement'));
+
+$$) where not exists (select * from migrations where id = '0004');
