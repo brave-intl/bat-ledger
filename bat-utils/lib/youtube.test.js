c22a5791b71aa9fb9d3fb0e556ffa9027f3e8422
Nick von Pentz <nickvonpentz@gmail.com>
2019-03-02 15:23:22 -0500

    Don't attempt to insert votes into tx if yt channel id can't be found

--- bat-utils/lib/youtube.test.js
+++ bat-utils/lib/youtube.test.js
@@ -9,3 +9,11 @@ test('retrieves the youtube channel id for a youtube username', async t => {
 
   t.true(retrievedChannelId === expectedChannelId)
 })
+
+test ('returns null for a non existant youtube usernname', async t => {
+  const fakeYoutubeUsername = '431a203d92969fb7d9009f4d975'
+  const expectedChannelId = null
+  const retrievedChannelId = (await getYoutubeChannelId(fakeYoutubeUsername))
+
+  t.true(retrievedChannelId === expectedChannelId)
+})
