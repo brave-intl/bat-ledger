a4479e08f3d609b90b4184e4f159c09067e5b047
Michael McLaughlin <mmclaughlin@brave.com>
2019-05-14 12:41:15 -0500

    added cascading for migrations

--- bin/migrate-down-all.sh
+++ bin/migrate-down-all.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+psql $DATABASE_URL -U eyeshade --single-transaction -v ON_ERROR_STOP=1 -f ./migrations/0008_on_delete_cascade/down.sql
+psql $DATABASE_URL -U eyeshade --single-transaction -v ON_ERROR_STOP=1 -f ./migrations/0007_potential_payments_ads/down.sql
+psql $DATABASE_URL -U eyeshade --single-transaction -v ON_ERROR_STOP=1 -f ./migrations/0006_payout_reports_ads/down.sql
 psql $DATABASE_URL -U eyeshade --single-transaction -v ON_ERROR_STOP=1 -f ./migrations/0005_account_types/down.sql
 psql $DATABASE_URL -U eyeshade --single-transaction -v ON_ERROR_STOP=1 -f ./migrations/0004_manual_settlements/down.sql
 psql $DATABASE_URL -U eyeshade --single-transaction -v ON_ERROR_STOP=1 -f ./migrations/0003_ads/down.sql
