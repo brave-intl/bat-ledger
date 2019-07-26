f780ce9559df1ea0c577cdf966bc6c9c56f33eaa
Nick von Pentz <nickvonpentz@gmail.com>
2019-07-26 15:18:01 -0400

    Delete whitespace to force github to recognize the commit

--- eyeshade/migrations/0009_unique_payment_ids/up.sql
+++ eyeshade/migrations/0009_unique_payment_ids/up.sql
@@ -6,4 +6,3 @@ alter table potential_payments_ads
   add constraint unique_payment_id_payout_report_id unique(payment_id, payout_report_id);
 
 $$) where not exists (select * from migrations where id = '0009');
-
