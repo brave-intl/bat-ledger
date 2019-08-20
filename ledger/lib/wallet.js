b77e87cbe4962fb132433da1fc92007fd2e4f3b5
eV (㋎) <8796196+evq@users.noreply.github.com>
2019-08-20 20:15:54 +0000

    one more excluded grant for ads balance calculation (#708)

--- ledger/lib/wallet.js
+++ ledger/lib/wallet.js
@@ -5,7 +5,8 @@ const promotionIdExclusions = {
   '1a9f55c7-6d54-41c6-97cd-7b8c4a290641': true,
   'c7a12742-2c7c-4ffc-9732-0e601e844099': true,
   'f66eac41-22b1-4c11-94ce-9c504d0539d8': true,
-  '74bc56a0-f4f9-4ac5-84a7-65e9babc41ff': true
+  '74bc56a0-f4f9-4ac5-84a7-65e9babc41ff': true,
+  'bc4d2067-dfe6-4f9b-9bf7-5bd80ec99180': true
 }
 module.exports = {
   promotionIdExclusions,
