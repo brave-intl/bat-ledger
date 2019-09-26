f678fe49770487d9284787f58946f7c179e867c0
Michael McLaughlin <mmclaughlin@brave.com>
2019-09-26 13:18:41 -0500

    added seeding to migration

---
+++ bin/seed.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+psql $DATABASE_URL -U eyeshade --single-transaction -v ON_ERROR_STOP=1 -f ./migrations/0010_geo_referral/seeds/groups.sql
+psql $DATABASE_URL -U eyeshade --single-transaction -v ON_ERROR_STOP=1 -f ./migrations/0010_geo_referral/seeds/countries.sql
