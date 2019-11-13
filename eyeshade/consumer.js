650529c51c93966f057e773bd6a56b0c01164ae8
eV <ev@7pr.xyz>
2019-11-13 22:29:33 +0000

    create kafka ca and key files from env var if passed

--- eyeshade/consumer.js
+++ eyeshade/consumer.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv')
+const fs = require('fs')
 const utils = require('bat-utils')
 
 const config = require('../config.js')
@@ -21,6 +22,24 @@ config.cache = false
 config.postgres.schemaVersion = require('./migrations/current')
 
 const runtime = new Runtime(config)
+
+const kafkaSslCa = process.env.KAFKA_SSL_CA
+const kafkaSslCaLocation = process.env.KAFKA_SSL_CA_LOCATION
+
+if (kafkaSslCa) {
+  if (kafkaSslCaLocation && !fs.existsSync(kafkaSslCaLocation)) {
+    fs.writeFileSync(kafkaSslCaLocation, kafkaSslCa)
+  }
+}
+
+const kafkaSSlKey = process.env.KAFKA_SSL_KEY
+const kafkaSSlKeyLocation = process.env.KAFKA_SSL_KEY_LOCATION
+
+if (kafkaSSlKey) {
+  if (kafkaSSlKeyLocation && !fs.existsSync(kafkaSSlKeyLocation)) {
+    fs.writeFileSync(kafkaSSlKeyLocation, kafkaSSlKey)
+  }
+}
 
 suggestionsConsumer(runtime)
 runtime.kafka.consume()
