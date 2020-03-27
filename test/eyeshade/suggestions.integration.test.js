d466a5fab69e72ba3af4e5770fb3c358f1a22186
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2020-03-27 14:34:07 -0500

    filled in avro key (#829)

--- test/eyeshade/suggestions.integration.test.js
+++ test/eyeshade/suggestions.integration.test.js
@@ -2,6 +2,7 @@
 
 const Kafka = require('bat-utils/lib/runtime-kafka')
 const test = require('ava')
+const uuidV4 = require('uuid/v4')
 const {
   timeout
 } = require('bat-utils/lib/extras-utils')
@@ -11,47 +12,47 @@ const {
   ok
 } = require('../utils')
 const Postgres = require('bat-utils/lib/runtime-postgres')
-const { suggestionType } = require('../../eyeshade/lib/suggestions')
+const suggestions = require('../../eyeshade/lib/suggestions')
 
 const postgres = new Postgres({ postgres: { url: process.env.BAT_POSTGRES_URL } })
-test.afterEach.always(async t => {
-  await cleanPgDb(postgres)()
-})
+test.beforeEach(cleanPgDb(postgres))
+test.afterEach.always(cleanPgDb(postgres))
 
 const channel = 'youtube#channel:UC2WPgbTIs9CDEV7NpX0-ccw'
-const example = {
-  id: 'e2874d25-14a9-4859-9729-78459af02a6f',
-  type: 'oneoff-tip',
-  channel: channel,
-  createdAt: (new Date()).toISOString(),
-  totalAmount: '10',
-  funding: [
-    {
-      type: 'ugp',
-      amount: '10',
-      cohort: 'control',
-      promotion: '6820f6a4-c6ef-481d-879c-d2c30c8928c3'
-    }
-  ]
-}
 const balanceURL = '/v1/accounts/balances'
 
 test('suggestions kafka consumer enters into votes', async (t) => {
   process.env.KAFKA_CONSUMER_GROUP = 'test-producer'
+  let body
   const runtime = {
     config: require('../../config')
   }
   const producer = new Kafka(runtime.config, runtime)
   await producer.connect()
 
-  let { body } = await agents.eyeshade.publishers.get(balanceURL)
+  const example = {
+    id: uuidV4(),
+    type: 'oneoff-tip',
+    channel: channel,
+    createdAt: (new Date()).toISOString(),
+    totalAmount: '10',
+    funding: [
+      {
+        type: 'ugp',
+        amount: '10',
+        cohort: 'control',
+        promotion: '6820f6a4-c6ef-481d-879c-d2c30c8928c3'
+      }
+    ]
+  }
+  ;({ body } = await agents.eyeshade.publishers.get(balanceURL)
     .query({
       pending: true,
       account: channel
-    }).expect(ok)
+    }).expect(ok))
   t.is(body.length, 0)
 
-  await producer.send(process.env.ENV + '.grant.suggestion', suggestionType.toBuffer(example))
+  await producer.send(process.env.ENV + '.grant.suggestion', suggestions.typeV1.toBuffer(example))
 
   while (!body.length) {
     await timeout(2000)
@@ -68,5 +69,90 @@ test('suggestions kafka consumer enters into votes', async (t) => {
     account_id: channel,
     account_type: 'channel',
     balance: '10.000000000000000000'
+  }], 'suggestion votes show up after small delay')
+
+  const exampleWithOrderId = {
+    id: uuidV4(),
+    type: 'oneoff-tip',
+    channel: channel,
+    createdAt: (new Date()).toISOString(),
+    totalAmount: '10',
+    orderId: uuidV4(),
+    funding: [
+      {
+        type: 'ugp',
+        amount: '10',
+        cohort: 'control',
+        promotion: '6820f6a4-c6ef-481d-879c-d2c30c8928c3'
+      }
+    ]
+  }
+  ;({ body } = await agents.eyeshade.publishers.get(balanceURL)
+    .query({
+      pending: true,
+      account: channel
+    }).expect(ok))
+  t.is(body.length, 1)
+
+  await producer.send(process.env.ENV + '.grant.suggestion', suggestions.typeV2.toBuffer(exampleWithOrderId))
+
+  body = [{}]
+  while (+body[0].balance !== 20) {
+    await timeout(2000)
+    ;({
+      body
+    } = await agents.eyeshade.publishers.get(balanceURL)
+      .query({
+        pending: true,
+        account: channel
+      })
+      .expect(ok))
+  }
+  t.deepEqual(body, [{
+    account_id: channel,
+    account_type: 'channel',
+    balance: '20.000000000000000000'
+  }], 'suggestion votes show up after small delay')
+
+  const exampleWithoutOrderId = {
+    id: uuidV4(),
+    type: 'oneoff-tip',
+    channel: channel,
+    createdAt: (new Date()).toISOString(),
+    totalAmount: '10',
+    funding: [
+      {
+        type: 'ugp',
+        amount: '10',
+        cohort: 'control',
+        promotion: '6820f6a4-c6ef-481d-879c-d2c30c8928c3'
+      }
+    ]
+  }
+  ;({ body } = await agents.eyeshade.publishers.get(balanceURL)
+    .query({
+      pending: true,
+      account: channel
+    }).expect(ok))
+  t.is(body.length, 1)
+
+  await producer.send(process.env.ENV + '.grant.suggestion', suggestions.typeV2.toBuffer(exampleWithoutOrderId))
+
+  body = [{}]
+  while (+body[0].balance !== 30) {
+    await timeout(2000)
+    ;({
+      body
+    } = await agents.eyeshade.publishers.get(balanceURL)
+      .query({
+        pending: true,
+        account: channel
+      })
+      .expect(ok))
+  }
+  t.deepEqual(body, [{
+    account_id: channel,
+    account_type: 'channel',
+    balance: '30.000000000000000000'
   }], 'suggestion votes show up after small delay')
 })
