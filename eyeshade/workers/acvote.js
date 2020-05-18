5157e360a148b3f486e2e3cdfadd180d8ff1e23a
Ben Huson <husobee@e-ie.io>
2020-05-18 16:54:29 -0400

    moving commit inside of try/catch and adding logging if no votes where commited

--- eyeshade/workers/acvote.js
+++ eyeshade/workers/acvote.js
@@ -12,62 +12,56 @@ module.exports = (runtime, callback) => {
 
     try {
       await client.query('BEGIN')
-      try {
-        let message
-        for (message of messages) {
-          const buf = Buffer.from(message.value, 'binary')
-          let vote
-          try {
-            vote = voteType.fromBuffer(buf)
-          } catch (e) {
-            // If the event is not well formed, capture the error and continue
-            runtime.captureException(e, { extra: { topic: voteTopic, message: message } })
-            continue
-          }
+      let message
+      for (message of messages) {
+        const buf = Buffer.from(message.value, 'binary')
+        let vote
+        try {
+          vote = voteType.fromBuffer(buf)
+        } catch (e) {
+          // If the event is not well formed, capture the error and continue
+          runtime.captureException(e, { extra: { topic: voteTopic, message: message } })
+          continue
+        }
 
-          const surveyorId = date + '_' + vote.fundingSource
-          const cohort = 'control'
-          const tally = vote.voteTally
-          const voteValue = vote.baseVoteValue
-          const publisher = vote.channel
-          const id = vote.id
+        const surveyorId = date + '_' + vote.fundingSource
+        const cohort = 'control'
+        const tally = vote.voteTally
+        const voteValue = vote.baseVoteValue
+        const publisher = vote.channel
+        const id = vote.id
 
-          const surveyorUpdate = `
+        const surveyorUpdate = `
             insert into surveyor_groups (id, price, virtual) values ($1, $2, true)
             on conflict (id) do nothing;
             `
-          await client.query(surveyorUpdate, [
-            surveyorId,
-            voteValue
-          ])
+        await client.query(surveyorUpdate, [
+          surveyorId,
+          voteValue
+        ])
 
-          const voteUpdate = `
+        const voteUpdate = `
             insert into votes (id, cohort, tally, excluded, channel, surveyor_id) values ($1, $2, $3, $4, $5, $6)
             on conflict (id) do update set updated_at = current_timestamp, tally = votes.tally + $3;
             `
-          await client.query(voteUpdate, [
-            votesId(publisher, cohort, id),
-            cohort,
-            tally,
-            runtime.config.testingCohorts.includes(cohort),
-            publisher,
-            surveyorId
-          ])
-        }
-      } catch (e) {
-        await client.query('ROLLBACK', err => {
-          if (err) {
-            console.error('Error rolling back transaction', err.stack)
-          }
-        })
-        runtime.captureException(e, { extra: { topic: voteTopic } })
-        throw e
+        await client.query(voteUpdate, [
+          votesId(publisher, cohort, id),
+          cohort,
+          tally,
+          runtime.config.testingCohorts.includes(cohort),
+          publisher,
+          surveyorId
+        ])
       }
-      await client.query('COMMIT', err => {
-        if (err) {
-          console.error('Error committing transaction', err.stack)
-        }
-      })
+
+      const results = await client.query('COMMIT')
+      if (results.rowCount === 0) {
+        console.log('committing votes: commit had zero row count for vote insertion')
+      }
+    } catch (e) {
+      await client.query('ROLLBACK')
+      runtime.captureException(e, { extra: { topic: voteTopic } })
+      throw e
     } finally {
       client.release()
     }
