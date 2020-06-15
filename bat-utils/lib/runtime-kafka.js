db92e7480d8360beff9097062f1ff27947b9534b
Michael McLaughlin <mmclaughlin@brave.com>
2020-06-15 17:04:46 -0500

    skipped insertion if <= 0 and handled throws

--- bat-utils/lib/runtime-kafka.js
+++ bat-utils/lib/runtime-kafka.js
@@ -57,17 +57,26 @@ class Kafka {
       // parallel processing on topic level
       const topicPromises = Object.keys(batchOfMessages).map(async (topic) => {
         // parallel processing on partition level
-        const partitionPromises = Object.keys(batchOfMessages[topic]).map((partition) => {
+        const partitionPromises = Object.keys(batchOfMessages[topic]).map(async (partition) => {
           // sequential processing on message level (to respect ORDER)
           const messages = batchOfMessages[topic][partition]
 
           debug('batch', topic, messages.length, messages[0])
-          return this.topicHandlers[topic](messages)
+          try {
+            await this.topicHandlers[topic](messages)
+          } catch (e) {
+            debug('batcherror', e)
+            return e
+          }
         })
 
         // wait until all partitions of this topic are processed and commit its offset
         // make sure to keep batch sizes large enough, you dont want to commit too often
-        await Promise.all(partitionPromises)
+        const results = await Promise.all(partitionPromises)
+        if (results.find((e) => e)) {
+          // if we find a truthy value (an error), don't update the topic
+          return
+        }
         await consumer.commitLocalOffsetsForTopic(topic)
       })
 
