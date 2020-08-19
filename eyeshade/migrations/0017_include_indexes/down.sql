1dcaf97968dca4ed05d872e3cedcf07472879dfb
Michael McLaughlin <mmclaughlin@brave.com>
2020-08-19 10:37:17 -0500

    updated postgres to be >11

--- eyeshade/migrations/0017_include_indexes/down.sql
+++ eyeshade/migrations/0017_include_indexes/down.sql
@@ -1,7 +1,6 @@
 select execute($$
 
 DROP INDEX IF EXISTS transactions_with_type_filter_idx;
-DROP INDEX IF EXISTS transactions_filter_idx;
 
 delete from migrations where id = '0017';
 
--- eyeshade/migrations/0017_include_indexes/up.sql
+++ eyeshade/migrations/0017_include_indexes/up.sql
@@ -2,10 +2,6 @@ select execute($$
 
 insert into migrations (id, description) values ('0017', 'include_indexes');
 
-CREATE INDEX transactions_filter_idx
-ON transactions (from_account, to_account)
-INCLUDE (created_at, description, channel, amount, settlement_currency, settlement_amount, transaction_type);
-
 CREATE INDEX transactions_with_type_filter_idx
 ON transactions (from_account, to_account, transaction_type)
 INCLUDE (created_at, description, channel, amount, settlement_currency, settlement_amount);
