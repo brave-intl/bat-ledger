affe705bb6dd2988d4d700b65f308b69fb89698b
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2020-10-07 15:25:18 -0500

    added migration for inserted at and modified tests (#945)
    * added migration for inserted at and modified tests
    
    * removed timestamp update

---
+++ eyeshade/migrations/0019_add_inserted_at/up.sql
@@ -0,0 +1,10 @@
+select execute($$
+
+insert into migrations (id, description) values ('0019', 'add_inserted_at');
+
+alter table transactions add column
+inserted_at timestamp with time zone
+not null
+default current_timestamp;
+
+$$) where not exists (select * from migrations where id = '0019');
