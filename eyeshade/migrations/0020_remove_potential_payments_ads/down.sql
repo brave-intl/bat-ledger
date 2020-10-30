6edbb295d67a29ac5ffa7ade2e4ae7b14d600009
Michael McLaughlin <mmclaughlin@brave.com>
2020-10-30 11:39:05 -0500

    moved table

--- eyeshade/migrations/0020_remove_potential_payments_ads/down.sql
+++ eyeshade/migrations/0020_remove_potential_payments_ads/down.sql
@@ -2,17 +2,17 @@ select execute($$
 
 delete from migrations where id = '0020';
 
+create table payout_reports_ads(
+  id         uuid primary key default uuid_generate_v4(),
+  created_at timestamp with time zone not null default current_timestamp
+);
+
 create table potential_payments_ads(
   id               uuid primary key default uuid_generate_v4(),
   payout_report_id uuid not null references payout_reports_ads(id),
   payment_id       uuid not null,
   provider_id      uuid not null,
   amount           numeric(28, 18) not null check (amount > 0.0),
-  created_at timestamp with time zone not null default current_timestamp
-);
-
-create table payout_reports_ads(
-  id         uuid primary key default uuid_generate_v4(),
   created_at timestamp with time zone not null default current_timestamp
 );
 
