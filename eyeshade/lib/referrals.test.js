4959ed4444826e251c597c3641abf0afbc6ef7d0
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2020-11-11 12:13:25 -0600

    Disable mongo (#964)
    * connect producer
    
    * removed indices creation
    
    * removed from config
    
    * created certs script
    
    * added cert files and removed more database references
    
    * removed tests and updated schemas
    
    * removed persona code and e2e
    
    * remove unused endpoints

--- eyeshade/lib/referrals.test.js
+++ eyeshade/lib/referrals.test.js
@@ -4,7 +4,7 @@ const utils = require('../../test/utils')
 
 test('referrals should be serializable and decodable without losing information', (t) => {
   const referral = utils.referral.create()
-  const buf = referrals.type.v1.toBuffer(referral)
+  const buf = referrals.encode(referral)
   t.true(Buffer.isBuffer(buf), 'should be serializable into a buffer')
   const decoded = referrals.decode(buf)
   t.is(decoded.version, 1, 'should be version 1 by default')
