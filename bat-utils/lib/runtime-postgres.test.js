4b9d6fccbfa9788ec5e09a20574a3d89b41cf353
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2021-03-17 16:54:47 -0500

    Batch surveyor txs (#1013)
    * batched serial transaction into single query
    
    * refactored insert prep
    
    * removed unnecessary check
    
    * converted to map completely
    
    * removed workflow_dispatch
    
    * added tests for the two untested levels
    
    * fixed implementation and used prepinsert inside of insert
    
    * made code for insertfromvoting dry
    
    * utilized pg-format and removed extra tests
    
    * updated batch size var

---
+++ bat-utils/lib/runtime-postgres.test.js
@@ -0,0 +1,34 @@
+const test = require('ava')
+const _ = require('underscore')
+const Postgres = require('./runtime-postgres')
+const { v4: uuidV4 } = require('uuid')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const postgres = new Postgres({
+  postgres: {
+    url: process.env.DATABASE_URL
+  }
+})
+
+test('instantiates correctly', (t) => {
+  t.true(_.isObject(postgres))
+})
+
+test('executes queries', async (t) => {
+  const id = uuidV4()
+  const { rows } = await postgres.query(`
+  insert into surveyor_groups (id, price, frozen, virtual)
+  values ($1, $2, $3, $4)
+  returning *`, [
+    id,
+    '0.25',
+    'false',
+    'true'
+  ])
+  t.is(rows.length, 1)
+  const { rows: gotten } = await postgres.query(`
+  select * from surveyor_groups
+  where id = $1`, [id])
+  t.deepEqual(rows, gotten)
+})
