e0b2eb6fa7d840b1618ba78026d3504bb08889cb
Michael McLaughlin <mmclaughlin@brave.com>
2021-03-18 12:34:57 -0500

    exposed acvote insert function and used in surveyors test

--- eyeshade/workers/acvote.js
+++ eyeshade/workers/acvote.js
@@ -18,34 +18,40 @@ module.exports = (runtime) => {
         runtime.captureException(e, { extra: { topic: voteTopic, message: message } })
         continue
       }
-
-      const surveyorId = date + '_' + vote.fundingSource
-      const cohort = 'control'
-      const tally = vote.voteTally
-      const voteValue = vote.baseVoteValue
-      const publisher = vote.channel
-
-      const surveyorUpdate = `
-          insert into surveyor_groups (id, price, virtual) values ($1, $2, true)
-          on conflict (id) do nothing;
-          `
-      await runtime.postgres.query(surveyorUpdate, [
-        surveyorId,
-        voteValue
-      ], client)
-
-      const voteUpdate = `
-          insert into votes (id, cohort, tally, excluded, channel, surveyor_id) values ($1, $2, $3, $4, $5, $6)
-          on conflict (id) do update set updated_at = current_timestamp, tally = votes.tally + $3;
-          `
-      await runtime.postgres.query(voteUpdate, [
-        votesId(publisher, cohort, surveyorId),
-        cohort,
-        tally,
-        runtime.config.testingCohorts.includes(cohort),
-        publisher,
-        surveyorId
-      ], client)
+      await insertVote(runtime, date, vote, client)
     }
   })
+}
+
+module.exports.insertVote = insertVote
+
+async function insertVote (runtime, date, vote, client) {
+  const surveyorId = date + '_' + vote.fundingSource
+  const cohort = 'control'
+  const tally = vote.voteTally
+  const voteValue = vote.baseVoteValue
+  const publisher = vote.channel
+
+  const surveyorUpdate = `
+      insert into surveyor_groups (id, price, virtual) values ($1, $2, true)
+      on conflict (id) do nothing;
+      `
+  await runtime.postgres.query(surveyorUpdate, [
+    surveyorId,
+    voteValue
+  ], client)
+
+  const voteUpdate = `
+      insert into votes (id, cohort, tally, excluded, channel, surveyor_id) values ($1, $2, $3, $4, $5, $6)
+      on conflict (id) do update set updated_at = current_timestamp, tally = votes.tally + $3
+      returning *;
+      `
+  return runtime.postgres.query(voteUpdate, [
+    votesId(publisher, cohort, surveyorId),
+    cohort,
+    tally,
+    runtime.config.testingCohorts.includes(cohort),
+    publisher,
+    surveyorId
+  ], client)
 }
