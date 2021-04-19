00aa920036017b29b3c6c700503668b701887861
Michael McLaughlin <mmclaughlin@brave.com>
2021-04-19 15:35:23 -0500

    added from account

--- eyeshade/controllers/accounts.js
+++ eyeshade/controllers/accounts.js
@@ -105,6 +105,7 @@ ORDER BY created_at
           amount = amount.negated()
         }
         const transaction = {
+          from_account: fromAccount,
           channel: channel || '',
           created_at: createdAt,
           description,
@@ -167,6 +168,7 @@ ORDER BY created_at
       settlement_amount: braveJoi.string().numeric().optional().description('amount in settlement_currency'),
       settlement_destination_type: stringValidator.valid.apply(stringValidator, settlementDestinationTypes).optional().description('type of address settlement was paid to'),
       settlement_destination: Joi.string().optional().description('destination address of the settlement'),
+      from_account: Joi.string().optional().description('destination address of the settlement'),
       transaction_type: transactionTypesValidator.required()
     }))
   }
