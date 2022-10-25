7e0787320dc8254cd50fef50c7736f2ad6c67994
Tyler Smart <tsmart@brave.com>
2022-10-24 19:18:12 -0700

    Add publishers lib test

--- eyeshade/lib/publishers.js
+++ eyeshade/lib/publishers.js
@@ -1,23 +1,28 @@
-const braveHapi = require('../../bat-utils/lib/extras-hapi.js')
+const braveHapi = require('../../bat-utils/lib/extras-hapi.js');
 
 module.exports = {
   hasValidCountry
 }
 
-async function hasValidCountry (runtime, channel) {
+async function hasValidCountry (runtime, channel, post=braveHapi.wreck.post ) {
   const publishers = runtime.config.publishers
   const channelsToCheck = { channel_ids: [channel] }
-  const response = await braveHapi.wreck.post(publishers.url + '/api/v3/public/channels/allowed_countries', {
-    headers: {
-      Authorization: 'Bearer ' + (publishers.access_token),
-      'Content-Type': 'application/json'
-    },
-    payload: JSON.stringify(channelsToCheck)
-  })
-  const result = JSON.parse(response.toString())
-
-  if (result[channel]) {
+  let result;
+  try {
+    const response = await post(publishers.url + '/api/v3/public/channels/allowed_countries', {
+      headers: {
+        Authorization: 'Bearer ' + (publishers.access_token),
+        'Content-Type': 'application/json'
+      },
+      payload: JSON.stringify(channelsToCheck)
+    })
+    result = JSON.parse(response.toString())
+  } catch (e) {
+    runtime.captureException(e)
+    return true
+  }
+  if (Object.hasOwn(result, channel)) {
     return result[channel]
   }
-  return false
+  return true
 }
